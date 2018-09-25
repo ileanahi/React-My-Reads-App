a57a11b499b7102e742f95972a3f431b2d581770
Ileana Argyris <argyris.ileana@gmail.com>
2018-09-26 00:40:10 +0200

    Created the array to show the read books

--- src/MainPage.js
+++ src/MainPage.js
@@ -46,9 +46,15 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                      </li>
-
+                    {
+                      this.props.books
+                          .filter(book => book.shelf === 'read')
+                          .map(book => (
+                              <li key={book.id} >
+                                  <Book />
+                              </li>
+                          ))
+                    }
                     </ol>
                   </div>
                 </div>
