3dbfbc59c711e1acc1ccb9b8cb0d611ea0188be1
Ileana Argyris <argyris.ileana@gmail.com>
2018-09-26 01:29:50 +0200

    Added moveShelf to books

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends Component {
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
                             <div className="book-shelf-changer">
                               <select
-                              onChange=()
+                              onChange={this.props.moveShelf()}
                               >
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
