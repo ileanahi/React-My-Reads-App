4d7d656dcf6c127b0a17eb19e73469a07de0ca28
Ileana Argyris <argyris.ileana@gmail.com>
2018-09-26 00:05:40 +0200

    Added state for books

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 import SearchPage from './SearchPage';
 import MainPage from './MainPage';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getall().then((books) => {
+      this.setState({ books })
+    })
+  }
 
   render() {
     return (
