3d5c89969dd06c041e0548ac3a27f640e86a2035
Ileana Argyris <argyris.ileana@gmail.com>
2018-09-26 18:57:32 +0200

    Grabbed value from search input

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -19,7 +19,10 @@ class SearchPage extends Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
+                <input
+                    type="text"
+                    placeholder="Search by title or author"
+                    value={this.state.query}/>
 
               </div>
             </div>
